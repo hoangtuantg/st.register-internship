5066a1bc35d9f945eba8cc7a8f20c4a02324b6f7
Trương Tuấn Hoàng <truongtuanhung09@gmail.com>
2025-07-12 14:21:03 +0700

    campaign.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,11 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.scss', 'resources/js/app.js','resources/css/auth.scss',],
+            input: [
+                'resources/css/app.scss', 
+                'resources/js/app.js',
+                'resources/css/auth.scss',
+                'resources/js/campaign/index.js'],
             refresh: true,
         }),
         tailwindcss(),
